b04c07081516b7c62a5210052c4e437aa8c90d6a
doongdoongpanda <101681393+doongdoongpanda@users.noreply.github.com>
2022-06-09 13:55:01 +0900

    hello

--- j/Algo1.java
+++ j/Algo1.java
@@ -3,22 +3,27 @@ package j;
 import java.util.Scanner;
 
 public class Algo1 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int count = scanner.nextInt();
-        System.out.println("Hello "+resolve(count));
-    }
-    public static String resolve(int count){
-        String nameAll = "";
-        for(int i=0 ; i < count ; i++){
-        Scanner scan = new Scanner(System.in);
-        String name = scan.next();
-        if( i <count-1) {
-        nameAll += name +",";
-        }else {
-        	 nameAll += name +".";
-        }
-        }
-        return nameAll;
-    }
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int count = scanner.nextInt();
+
+		System.out.println("Hello " + resolve(count));
+		scanner.close();
+	}
+
+	public static String resolve(int count) {
+		String nameAll = "";
+		Scanner scan = new Scanner(System.in);
+		for (int i = 0; i < count; i++) {
+
+			String name = scan.next();
+			if (i < count - 1) {
+				nameAll += name + ",";
+			} else {
+				nameAll += name + ".";
+			}
+		}
+		scan.close();
+		return nameAll;
+	}
 }
