1c4d53d788c816f670eaee82211b9665ac819528
doongdoongpanda <101681393+doongdoongpanda@users.noreply.github.com>
2022-06-09 13:01:33 +0900

    Vowel

---
+++ j/word.java
@@ -0,0 +1,46 @@
+package exam;
+
+public class word {
+	public static void main(String[] args) {
+		//
+		word word = new word("Hello World");
+
+		System.out.println(word.isVowel1(1));
+		System.out.println(word.isVowel2(1));
+		System.out.println(word.isVowel3(1));
+
+
+	}
+	private String letters;
+
+	public word(String letters) {
+		this.letters = letters.toLowerCase();
+	}
+	
+
+	public boolean isVowel1(int i) {
+		return letters.substring(i, i+1).equals("a") 
+				|| letters.substring(i, i+1).equals("i")
+				|| letters.substring(i, i+1).equals("u")
+				|| letters.substring(i, i+1).equals("e")
+				|| letters.substring(i, i+1).equals("o");
+	}
+	
+	public boolean isVowel2(int i) {
+		String vowel = "aiueo";
+		return vowel.contains(letters.substring(i, i+1));
+	}
+	
+	public boolean isVowel3(int i) {
+		String [] vowel = {"a", "i", "u", "e", "o"};
+		String word = letters.substring(i, i+1);
+		boolean answer = false;
+		for(int j = 0; j < vowel.length; j++) {
+			if(word.equals(vowel[0]) || word.equals(vowel[1]) || word.equals(vowel[2]) || word.equals(vowel[3])
+					 || word.equals(vowel[4])) {
+				answer = true;
+			} 
+		}
+		return answer;
+	}
+}
