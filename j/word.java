381525f7a94b10efd222629231f04d8d57374912
doongdoongpanda <101681393+doongdoongpanda@users.noreply.github.com>
2022-06-09 13:17:07 +0900

    ! 버전으로 자음추가

--- j/word.java
+++ j/word.java
@@ -9,63 +9,67 @@ public class word {
 		System.out.println(word.isVowel2(1));
 		System.out.println(word.isVowel3(1));
 		System.out.println(word.isConsonat(1));
-		System.out.println(word.isConsonat(2));
+		System.out.println(word.isConsonat(1));
+		System.out.println(word.isConsonat(1));
 
 	}
+
 	private String letters;
 
 	public word(String letters) {
 		this.letters = letters.toLowerCase();
 	}
-	
 
 	public boolean isVowel1(int i) {
-		return letters.substring(i, i+1).equals("a") 
-				|| letters.substring(i, i+1).equals("i")
-				|| letters.substring(i, i+1).equals("u")
-				|| letters.substring(i, i+1).equals("e")
-				|| letters.substring(i, i+1).equals("o");
+		return letters.substring(i, i + 1).equals("a") || letters.substring(i, i + 1).equals("i")
+				|| letters.substring(i, i + 1).equals("u") || letters.substring(i, i + 1).equals("e")
+				|| letters.substring(i, i + 1).equals("o");
 	}
-	
+
 	public boolean isVowel2(int i) {
 		String vowel = "aiueo";
-		return vowel.contains(letters.substring(i, i+1));
+		return vowel.contains(letters.substring(i, i + 1));
 	}
-	
+
 	public boolean isVowel3(int i) {
-		String [] vowel = {"a", "i", "u", "e", "o"};
-		String word = letters.substring(i, i+1);
+		String[] vowel = { "a", "i", "u", "e", "o" };
+		String word = letters.substring(i, i + 1);
 		boolean answer = false;
-		for(int j = 0; j < vowel.length; j++) {
-			if(word.equals(vowel[0]) || word.equals(vowel[1]) || word.equals(vowel[2]) || word.equals(vowel[3])
-					 || word.equals(vowel[4])) {
+		for (int j = 0; j < vowel.length; j++) {
+			if (word.equals(vowel[0]) || word.equals(vowel[1]) || word.equals(vowel[2]) || word.equals(vowel[3])
+					|| word.equals(vowel[4])) {
 				answer = true;
-			} 
+			}
 		}
-		return answer;
-	}
-	
-	//자음 확인
-    public boolean isConsonat(int i) {
-    	String [] vowel = {"a", "i", "u", "e", "o"};
-		String word = letters.substring(i, i+1);
-		boolean answer = true;
-		for(int j = 0; j < vowel.length; j++) {
-			if(word.equals(vowel[0]) || word.equals(vowel[1]) || word.equals(vowel[2]) || word.equals(vowel[3])
-					 || word.equals(vowel[4])) {
-				answer = false;
-			} 
-		}
-		return answer;
-	}
-	//자음 확인2
-    public boolean isConsonat2(int i) {
-    	String vowel = "aiueo";
-		boolean answer = true;
-			if(vowel.contains(letters.substring(i, i+1))) {
-				answer = false;
-			} 
 		return answer;
 	}
 
+	// 자음 확인
+	public boolean isConsonat(int i) {
+		String[] vowel = { "a", "i", "u", "e", "o" };
+		String word = letters.substring(i, i + 1);
+		boolean answer = true;
+		for (int j = 0; j < vowel.length; j++) {
+			if (word.equals(vowel[0]) || word.equals(vowel[1]) || word.equals(vowel[2]) || word.equals(vowel[3])
+					|| word.equals(vowel[4])) {
+				answer = false;
+			}
+		}
+		return answer;
+	}
+
+	// 자음 확인2
+	public boolean isConsonat2(int i) {
+		String vowel = "aiueo";
+		boolean answer = true;
+		if (vowel.contains(letters.substring(i, i + 1))) {
+			answer = false;
+		}
+		return answer;
+	}
+
+	// 자음 확인3
+	public boolean isConsonat3(int i) {
+		return !isVowel3(i);
+	}
 }
